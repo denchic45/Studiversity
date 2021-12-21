ab9c89ff3c02fd9349e116006dfe5c5335fd4cc5
Denis <denchic150@gmail.com>
2021-12-21 19:38:13 +0300

    Some commit

--- app/src/main/java/com/denchic45/kts/data/model/mapper/TaskMapper.java
+++ app/src/main/java/com/denchic45/kts/data/model/mapper/TaskMapper.java
@@ -7,7 +7,9 @@ import com.denchic45.kts.data.model.room.TaskEntity;
 import org.mapstruct.Mapper;
 
 @Mapper
-public interface TaskMapper extends DomainEntityMapper<Task, TaskEntity>, DocEntityMapper<TaskDoc,TaskEntity>{
+public interface TaskMapper extends DomainEntityMapper<Task, TaskEntity>,
+        DocEntityMapper<TaskDoc,TaskEntity>,
+        DomainDocMapper<Task,TaskDoc>{
     @Override
     TaskEntity domainToEntity(Task domain);
 
