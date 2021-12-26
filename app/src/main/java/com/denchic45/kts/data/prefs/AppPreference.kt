6fd2f95a63a69cee29a18d33cd5911cacfa57b60
Denis <denchic150@gmail.com>
2021-12-27 00:16:22 +0300

    Add new pref 'COURSES_LOADED_FIRST_TIME'

--- app/src/main/java/com/denchic45/kts/data/prefs/AppPreference.kt
+++ app/src/main/java/com/denchic45/kts/data/prefs/AppPreference.kt
@@ -6,11 +6,18 @@ import javax.inject.Inject
 class AppPreference @Inject constructor(context: Context) : BaseSharedPreference(context, "App") {
     var lessonTime: Int
         get() = getValue(LESSON_TIME, 40)
-        set(lessonTime) {
-            setValue(LESSON_TIME, lessonTime)
+        set(value) {
+            setValue(LESSON_TIME, value)
+        }
+
+    var coursesLoadedFirstTime: Boolean
+        get() = getValue(COURSES_LOADED_FIRST_TIME, false)
+        set(value) {
+            setValue(COURSES_LOADED_FIRST_TIME, value)
         }
 
     companion object {
         const val LESSON_TIME = "LESSON_TIME"
+        const val COURSES_LOADED_FIRST_TIME = "COURSES_LOADED_FIRST_TIME"
     }
 }
