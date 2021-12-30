9f79edd4244c7c8ebcd02dd8438e8b18be101f35
Denis <denchic150@gmail.com>
2021-12-30 15:53:35 +0300

    Add attachments

--- app/src/main/java/com/denchic45/kts/data/model/domain/Task.kt
+++ app/src/main/java/com/denchic45/kts/data/model/domain/Task.kt
@@ -1,23 +1,27 @@
 package com.denchic45.kts.data.model.domain
 
 import com.denchic45.kts.data.model.DomainModel
+import java.io.File
 import java.util.*
 
 data class Task(
     override var uuid: String,
     val courseUuid: String,
-    val sectionUuid:String,
+    val sectionUuid: String,
     val name: String,
     val content: String,
+    val attachments: List<Attachment>,
     val completionDate: Date,
     val createdDate: Date,
     val updatedDate: Date,
     val completed: Boolean,
 ) : DomainModel() {
 
-    private constructor():this("","", "","","",Date(0),Date(0), Date(),false)
+    private constructor() : this("", "", "", "", "", emptyList(), Date(0), Date(0), Date(), false)
 
     companion object {
         fun createEmpty() = Task()
     }
 }
+
+data class Attachment(val file: File) : DomainModel()
