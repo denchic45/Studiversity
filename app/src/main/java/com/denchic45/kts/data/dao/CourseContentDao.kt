52d28ec57b5a3774b51bc8433687a697484ad0d0
Denis <denchic150@gmail.com>
2022-01-10 23:08:12 +0300

    Add query

--- app/src/main/java/com/denchic45/kts/data/dao/CourseContentDao.kt
+++ app/src/main/java/com/denchic45/kts/data/dao/CourseContentDao.kt
@@ -9,8 +9,11 @@ import kotlinx.coroutines.flow.Flow
 abstract class CourseContentDao : BaseDao<CourseContentEntity>() {
 
     @Query("SELECT * FROM course_content WHERE courseId =:courseId")
-    abstract fun getByCourseUuid(courseId: String) :Flow<List<CourseContentEntity>>
+    abstract fun getByCourseUuid(courseId: String): Flow<List<CourseContentEntity>>
 
     @Query("SELECT * FROM course_content WHERE content_id=:id")
     abstract fun get(id: String): Flow<CourseContentEntity>
+
+    @Query("SELECT cc.attachments FROM course_content cc WHERE content_id=:id")
+    abstract fun getAttachmentsById(id: String): Flow<String>
 }
