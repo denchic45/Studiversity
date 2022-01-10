bc501a3253a9748e98c6a7a617cf90456351bfb9
Denis <denchic150@gmail.com>
2022-01-10 23:16:21 +0300

    Refactor code

--- app/src/main/java/com/denchic45/kts/data/model/room/ListConverter.kt
+++ app/src/main/java/com/denchic45/kts/data/model/room/ListConverter.kt
@@ -5,11 +5,15 @@ import androidx.room.TypeConverter
 object ListConverter {
     @TypeConverter
     fun fromList(list: List<String>): String {
-        return list.joinToString { "," }
+        return list.joinToString(",")
     }
 
     @TypeConverter
     fun tolList(s: String): List<String> {
-        return listOf(*s.split(",".toRegex()).toTypedArray())
+        return if (s.isEmpty())
+            emptyList()
+        else
+            listOf(*s.split(",".toRegex()).toTypedArray())
     }
+
 }
