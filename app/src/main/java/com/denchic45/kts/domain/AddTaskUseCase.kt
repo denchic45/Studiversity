73b3a882bdcdf0c49053004495834ec5f68a2c15
Denis <denchic150@gmail.com>
2022-01-10 10:53:39 +0300

    Add usecase

---
+++ app/src/main/java/com/denchic45/kts/domain/AddTaskUseCase.kt
@@ -0,0 +1,14 @@
+package com.denchic45.kts.domain
+
+import com.denchic45.kts.data.model.domain.Task
+import com.denchic45.kts.data.repository.CourseRepository
+import javax.inject.Inject
+
+class AddTaskUseCase @Inject constructor(
+    private val courseRepository: CourseRepository
+) {
+    suspend operator fun invoke(task: Task){
+        courseRepository.addTask(task)
+    }
+
+}
