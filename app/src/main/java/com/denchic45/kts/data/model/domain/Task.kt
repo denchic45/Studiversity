e6bbd36b7aba5ae0b0b99115ec1fa33b758789ca
Denis <denchic150@gmail.com>
2022-01-10 11:18:39 +0300

    add and update fields

--- app/src/main/java/com/denchic45/kts/data/model/domain/Task.kt
+++ app/src/main/java/com/denchic45/kts/data/model/domain/Task.kt
@@ -1,36 +1,50 @@
 package com.denchic45.kts.data.model.domain
 
 import com.denchic45.kts.data.model.DomainModel
-import com.denchic45.kts.utils.Files
-import java.io.File
-import java.util.*
-import android.provider.MediaStore
-
-import android.media.ThumbnailUtils
-
-import android.graphics.Bitmap
-import android.os.Build
-import android.os.CancellationSignal
-import android.util.Size
-import android.webkit.MimeTypeMap
 import com.denchic45.kts.utils.UUIDS
 import com.denchic45.kts.utils.getExtension
+import java.io.File
+import java.time.LocalDateTime
+import java.util.*
 
 
 data class Task(
     override var uuid: String,
-    val courseId: String,
-    val sectionId: String,
-    val name: String,
-    val content: String,
+    override val courseId: String,
+    override val sectionId: String,
+    override val name: String,
+    override val description: String,
+    val completionDate: LocalDateTime,
+    val disabledSendAfterDate: Boolean,
     val attachments: List<Attachment>,
-    val completionDate: Date,
-    val createdDate: Date,
-    val updatedDate: Date,
-    val completed: Boolean,
-) : DomainModel() {
+    val answerType: AnswerType,
+    val markType: MarkType,
+    override val commentsEnabled: Boolean,
+    override val createdDate: Date,
+    override val timestamp: Date,
+) : CourseContent() {
 
-    private constructor() : this("", "", "", "", "", emptyList(), Date(0), Date(0), Date(), false)
+    private constructor() : this(
+        "",
+        "",
+        "",
+        "",
+        "",
+        LocalDateTime.now(),
+        false,
+        emptyList(),
+        AnswerType(
+            true,
+            100,
+            false,
+            16,
+            200
+        ),
+        MarkType.Score(5),
+        false,
+        Date(),
+        Date()
+    )
 
     companion object {
         fun createEmpty() = Task()
@@ -40,10 +54,29 @@ data class Task(
 data class Attachment(
     override var uuid: String = UUIDS.createShort(),
     val file: File
-    ) : DomainModel() {
+) : DomainModel() {
 
     val name: String = file.name
 
     val extension: String = file.getExtension()
+}
 
+data class AnswerType(
+    val textAvailable: Boolean,
+    val charsLimit: Int,
+    val attachmentsAvailable: Boolean,
+    val attachmentsLimit: Int,
+    val attachmentsSizeLimit: Int
+)
+
+sealed class MarkType {
+    abstract val position: Int
+
+    data class Score(val maxScore: Int) : MarkType() {
+        override val position: Int get() = 0
+    }
+
+    object Binary : MarkType() {
+        override val position: Int get() = 1
+    }
 }
