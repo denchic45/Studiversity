830fe32651b305c3a652e61486285a362a0bf237
Denis <denchic150@gmail.com>
2022-01-10 11:20:07 +0300

    add and update fields

---
+++ app/src/main/java/com/denchic45/kts/data/model/firestore/CourseContentDoc.kt
@@ -0,0 +1,38 @@
+package com.denchic45.kts.data.model.firestore
+
+import com.denchic45.kts.data.model.DocModel
+import com.denchic45.kts.data.model.domain.ContentDetails
+import com.denchic45.kts.data.model.domain.ContentType
+import com.denchic45.kts.data.model.mapper.Default
+import com.google.firebase.firestore.PropertyName
+import java.util.*
+
+data class CourseContentDoc @Default @JvmOverloads constructor(
+    val id: String,
+    val courseId: String = "",
+    val sectionId: String = "",
+    val name: String,
+    val description: String,
+    val commentsEnabled: Boolean,
+    val completionDate: Date,
+    val createdDate: Date,
+    val timestamp: Date,
+    @get:PropertyName("details")
+    val contentDetails: ContentDetails,
+    val contentType: ContentType,
+    val completions: List<CompletionTask> = emptyList()
+) : DocModel {
+    private constructor() : this(
+        "", "", "", "", "",
+        false, Date(), Date(), Date(), ContentDetails.Empty, ContentType.TASK,
+        emptyList()
+    )
+}
+
+data class CompletionTask(
+    val studentUuid: String,
+    val teacherUuid: String,
+    val completedDate: Date,
+    val grade: Int,
+    val assessmentDate: Date,
+)
---
+++ app/src/main/java/com/denchic45/kts/data/model/room/CourseContentEntity.kt
@@ -0,0 +1,31 @@
+package com.denchic45.kts.data.model.room
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.denchic45.kts.data.model.EntityModel
+import com.denchic45.kts.data.model.domain.ContentType
+import java.util.*
+
+@Entity(tableName = "course_content")
+data class CourseContentEntity(
+    @field:PrimaryKey
+    @field:ColumnInfo(name = "content_id")
+    var id: String,
+    var courseId: String,
+    val sectionId: String,
+    val name: String,
+    val description: String,
+    val details: String,
+    val commentsEnabled: Boolean,
+    val contentType: ContentType,
+    @field:TypeConverters(DateConverter::class)
+    @field:ColumnInfo(name = "date_created")
+    val createdDate: Date,
+    @field:TypeConverters(TimestampConverter::class)
+    val timestamp: Date,
+    @field:TypeConverters(DateConverter::class)
+    @field:ColumnInfo(name = "date_completion")
+    val completionDate: Date
+) : EntityModel
