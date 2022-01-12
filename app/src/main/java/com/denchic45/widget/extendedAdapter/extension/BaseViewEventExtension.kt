e3fdb31c20171b57003a8d9cae18577952290658
Denis <denchic150@gmail.com>
2022-01-12 22:44:25 +0300

    Create abstract extension for view events extensions

---
+++ app/src/main/java/com/denchic45/widget/extendedAdapter/extension/BaseViewEventExtension.kt
@@ -0,0 +1,35 @@
+package com.denchic45.widget.extendedAdapter.extension
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.denchic45.widget.extendedAdapter.AdapterDelegateExtension
+import com.denchic45.widget.extendedAdapter.DelegationAdapterDsl
+import com.denchic45.widget.extendedAdapter.IDelegationAdapterExtended
+
+
+
+abstract class BaseViewEventExtension<VH : RecyclerView.ViewHolder>(
+    private val vhClassType: Class<VH>,
+    private val onClick: (position: Int) -> Unit,
+    private val predicate: (VH) -> Boolean = { true },
+    val view: (VH) -> View
+) : AdapterDelegateExtension {
+
+    private fun isNotCorrectType(viewHolder: RecyclerView.ViewHolder): Boolean {
+        return vhClassType != viewHolder::class.java
+    }
+
+    override fun onAttach(
+        adapterExtended: IDelegationAdapterExtended
+    ) {
+        adapterExtended.adapterEventEmitter.addOnCreateObserver { viewHolder ->
+            if (adapterExtended.delegatesCount() != 1 && isNotCorrectType(viewHolder))
+                return@addOnCreateObserver
+            if (predicate(viewHolder as VH)) {
+                onViewDone(viewHolder)
+            }
+        }
+    }
+
+    abstract fun onViewDone(viewHolder: VH)
+}
