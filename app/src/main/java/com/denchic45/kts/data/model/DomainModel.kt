18b2d6e0c6be17b1ce69af3af04fdb7ba41a8a4e
Denis <denchic150@gmail.com>
2022-01-25 18:50:50 +0300

    Override equals

--- app/src/main/java/com/denchic45/kts/data/model/DomainModel.kt
+++ app/src/main/java/com/denchic45/kts/data/model/DomainModel.kt
@@ -4,6 +4,10 @@ abstract class DomainModel : Equatable {
 
     open var id: String = ""
 
+    override fun equals(other: Any?): Boolean = throw IllegalStateException()
+
+    override fun hashCode(): Int = throw java.lang.IllegalStateException()
+
     open fun copy(): DomainModel {
         throw IllegalStateException("You must override copy function")
     }
