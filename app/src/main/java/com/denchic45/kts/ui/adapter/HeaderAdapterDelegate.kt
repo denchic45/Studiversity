3f446335049c21dadb19a3d696f6ef885fb1553d
Denis <denchic150@gmail.com>
2022-04-14 20:49:13 +0300

    Add header adapter delegate

---
+++ app/src/main/java/com/denchic45/kts/ui/adapter/HeaderAdapterDelegate.kt
@@ -0,0 +1,33 @@
+package com.denchic45.kts.ui.adapter
+
+import android.view.ViewGroup
+import com.denchic45.kts.data.model.ui.Header
+import com.denchic45.kts.databinding.ItemHeaderBinding
+import com.denchic45.kts.utils.viewBinding
+import com.denchic45.widget.extendedAdapter.ListItemAdapterDelegate
+
+class HeaderAdapterDelegate :
+    ListItemAdapterDelegate<Header, HeaderAdapterDelegate.HeaderHolder>() {
+
+    override fun isForViewType(item: Any): Boolean {
+        return item is Header
+    }
+
+    override fun onBindViewHolder(item: Header, holder: HeaderHolder) {
+        holder.onBind(item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup): HeaderHolder {
+        return HeaderHolder(parent.viewBinding(ItemHeaderBinding::inflate))
+    }
+
+    class HeaderHolder(itemHeaderBinding: ItemHeaderBinding) :
+        BaseViewHolder<Header, ItemHeaderBinding>(itemHeaderBinding) {
+
+        override fun onBind(item: Header) {
+            with(binding) {
+                tvHeader.text = item.title
+            }
+        }
+    }
+}
