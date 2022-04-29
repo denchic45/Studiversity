d897365dd34536f2615349358056dbe621c95430
Denis <denchic150@gmail.com>
2022-04-29 17:01:33 +0300

    Completed student list options

--- app/src/main/java/com/denchic45/kts/utils/Options.kt
+++ app/src/main/java/com/denchic45/kts/utils/Options.kt
@@ -1,14 +1,21 @@
 package com.denchic45.kts.utils
 
-import com.denchic45.kts.data.model.domain.ListItem
+import com.denchic45.kts.R
 import com.denchic45.kts.data.model.domain.OptionItem
+import com.denchic45.kts.data.model.ui.UiText
 
 class Options {
     companion object {
-        fun student() = listOf<ListItem>(
-//            OptionItem(
-//                title = "Посмотреть профиль"
-//            )
+        fun student() = listOf(
+            OptionItem(
+                title = UiText.IdText(R.string.option_set_headman)
+            ),
+            OptionItem(
+                title = UiText.IdText(R.string.option_edit_user)
+            ),
+            OptionItem(
+                title = UiText.IdText(R.string.option_remove_headman)
+            ),
         )
     }
 }
