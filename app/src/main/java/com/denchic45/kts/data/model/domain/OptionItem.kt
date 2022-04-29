38a5bad86e2d0e22988c619bebb4d0dadb3288a3
Denis <denchic150@gmail.com>
2022-04-29 17:01:46 +0300

    Change text field

--- app/src/main/java/com/denchic45/kts/data/model/domain/OptionItem.kt
+++ app/src/main/java/com/denchic45/kts/data/model/domain/OptionItem.kt
@@ -3,11 +3,12 @@ package com.denchic45.kts.data.model.domain
 import com.denchic45.kts.data.UiModel
 import com.denchic45.kts.data.model.ui.UiColor
 import com.denchic45.kts.data.model.ui.UiImage
+import com.denchic45.kts.data.model.ui.UiText
 
 data class OptionItem(
-    val title: String = "",
-    val icon: UiImage?,
-    val color: UiColor?,
+    val title: UiText,
+    val icon: UiImage? = null,
+    val color: UiColor? = null,
     val enable: Boolean = true
 ) : UiModel {
 
