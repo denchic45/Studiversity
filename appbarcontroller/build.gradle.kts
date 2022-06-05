ac3a0ee7dc957a3b69c217e4c60bee077acfa2b3
Denis <denchic150@gmail.com>
2022-06-05 18:41:04 +0300

    Migrate gradle to kts

---
+++ appbarcontroller/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.library")
+}
+apply(plugin = ("kotlin-android"))
+
+android {
+    compileSdk = 31
+    buildToolsVersion = "30.0.3"
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 31
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//        consumerProguardFiles = "consumer-rules.pro"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    namespace = "com.example.appbarcontroller"
+}
+
+dependencies {
+    implementation ("androidx.appcompat:appcompat:1.4.2")
+    implementation ("com.google.android.material:material:1.6.1")
+    implementation ("org.jetbrains:annotations:15.0")
+    implementation ("androidx.core:core-ktx:1.8.0")
+}
+repositories {
+    mavenCentral()
+}
