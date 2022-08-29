33c6eb1ab5a80c0a6080f19f772bbb46a9e53cc5
Denis <denchic150@gmail.com>
2022-08-29 22:56:28 +0300

    Update login screen

--- desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/login/LoginScreen.kt
+++ desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/login/LoginScreen.kt
@@ -1,12 +1,15 @@
 package com.denchic45.kts.ui.login
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.material.TextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
@@ -15,21 +18,26 @@ fun LoginScreen(loginComponent: LoginComponent) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
-    Surface {
-        Column {
+    Surface(modifier = Modifier.padding(16.dp).size(450.dp)) {
+        Column(verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
             TextField(
+                modifier = Modifier.padding(8.dp),
                 value = email,
                 onValueChange = { email = it },
-                label = { Text("Почта") }
+                label = { Text("Почта") },
             )
             TextField(
+                modifier = Modifier.padding(8.dp),
                 value = password,
                 onValueChange = { password = it },
                 label = { Text("Пароль") }
             )
             Button(
-                onClick = {}
-            ) {}
+                onClick = {loginComponent.onLoginClick("denchic860@gmail.com", "Den141819!kts")}
+            ) {
+                Text("Войти")
+            }
         }
     }
 }
