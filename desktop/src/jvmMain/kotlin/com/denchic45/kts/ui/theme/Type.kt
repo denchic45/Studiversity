6da661c8d7569d0aaf7a4452f53f9daace5688de
Denis <denchic150@gmail.com>
2022-08-29 22:58:50 +0300

    Update theme typography

--- desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/theme/Type.kt
+++ desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/theme/Type.kt
@@ -1,14 +1,11 @@
 package com.denchic45.kts.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.platform.Font
 import androidx.compose.ui.unit.ExperimentalUnitApi
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
-import androidx.compose.ui.unit.sp
 
 val defaultTypography = Typography()
 
@@ -17,38 +14,26 @@ val Typography = Typography(
     headlineMedium = defaultTypography.headlineMedium.copy(
         fontFamily = FontFamily(
             Font(resource = "fonts/Gilroy-Medium.ttf")
-        ),
+        )
+    ),
+    headlineSmall = defaultTypography.headlineSmall.copy(
+        fontFamily = FontFamily(
+            Font(resource = "fonts/Gilroy-Medium.ttf")
+        )
     ),
     titleLarge = defaultTypography.titleLarge.copy(
         fontFamily = FontFamily(
             Font(resource = "fonts/Gilroy-Semibold.ttf")
         ),
         fontSize = TextUnit(20F, TextUnitType.Sp)
-
-//        fontSize = TextUnit(22F, TextUnitType.Sp),
-//        lineHeight = TypeScale.TitleLargeLineHeight,
-//        letterSpacing = TypeScale.TitleLargeTracking,
     ),
     bodyLarge = defaultTypography.bodyLarge.copy(
-        fontFamily = FontFamily(
-            Font(resource = "fonts/Gilroy-Medium.ttf")
-        ),
+        fontFamily = FontFamily(Font(resource = "fonts/Gilroy-Medium.ttf"))
     ),
-    bodyMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+    bodyMedium = defaultTypography.bodyLarge.copy(
+        fontFamily = FontFamily(Font(resource = "fonts/Gilroy-Medium.ttf"))
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+    bodySmall = defaultTypography.bodySmall.copy(
+        fontFamily = FontFamily(Font(resource = "fonts/Gilroy-Medium.ttf"))
     )
-    */
 )
