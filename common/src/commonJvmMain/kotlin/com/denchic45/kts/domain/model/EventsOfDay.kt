dd530ee470171e3eb55a1840748c9e3657f57939
Denis <denchic150@gmail.com>
2022-09-17 13:13:40 +0300

    Change data class to class and remove internal field '_events'

--- common/src/commonJvmMain/kotlin/com/denchic45/kts/domain/model/EventsOfDay.kt
+++ common/src/commonJvmMain/kotlin/com/denchic45/kts/domain/model/EventsOfDay.kt
@@ -1,48 +1,45 @@
 package com.denchic45.kts.domain.model
 
+import com.denchic45.kts.util.UUIDS
 import com.denchic45.kts.util.swap
 import com.denchic45.kts.util.toString
 import java.time.LocalDate
 
-data class EventsOfDay(
+class EventsOfDay constructor(
     val date: LocalDate,
-    private val _events: List<Event> = mutableListOf(),
+    events: List<Event> = listOf(),
     val startsAtZero: Boolean = false,
     val id: String,
 ) {
 
-    val size: Int = _events.size
+    val events: List<Event> = removeRedundantEmptyEvents(events.onEach { it.eventsOfDay = this })
 
-    val events: List<Event> =
-        removeRedundantEmptyEvents(_events.onEach { it.eventsOfDay = this })
+    val size: Int = events.size
 
     val dayOfWeek: Int
         get() = date.dayOfWeek.value
 
-    fun add(event: Event): EventsOfDay = copy(_events = events.map(Event::copy) + event)
+    fun add(event: Event): EventsOfDay = copy(events.map(Event::copy) + event)
 
     fun add(event: Event, order: Int): EventsOfDay {
         return copy(
-            _events = events.map(Event::copy).toMutableList()
-                .apply { add(indexOfOrder(order), event) }
+            events.map(Event::copy).toMutableList().apply { add(indexOfOrder(order), event) }
         )
     }
 
     fun update(editedEvent: Event, index: Int = orderOf(editedEvent)): EventsOfDay {
-        return copy(_events = events.toMutableList().apply {
+        return copy(events.toMutableList().apply {
             set(indexOfOrder(index), editedEvent)
         })
     }
 
-    private fun indexOfOrder(order: Int): Int {
-        return order - offsetIndex
-    }
+    private fun indexOfOrder(order: Int): Int = order - offsetIndex
 
     private val offsetIndex: Int
         get() = if (startsAtZero) 0 else 1
 
     fun swap(oldIndex: Int, newIndex: Int): EventsOfDay {
-        return copy(_events = events.swap(oldIndex, newIndex).map(Event::copy))
+        return copy(events.swap(oldIndex, newIndex).map(Event::copy))
     }
 
     private fun removeRedundantEmptyEvents(events: List<Event>): List<Event> {
@@ -58,17 +55,16 @@ data class EventsOfDay(
                 break
             }
         }
-
         return updatedEvents
     }
 
     fun remove(event: Event): EventsOfDay {
-        return copy(_events = events.map(Event::copy) - event)
+        return copy(events.map(Event::copy) - event)
     }
 
-    fun isEmpty(): Boolean = _events.isEmpty()
+    fun isEmpty(): Boolean = events.isEmpty()
 
-    fun last(): Event = _events.last()
+    fun last(): Event = events.last()
 
     fun orderOf(event: Event): Int {
         return events.indexOfFirst { event.id == it.id } + offsetIndex
@@ -77,7 +73,10 @@ data class EventsOfDay(
     val weekName: String
         get() = date.toString("EEE")
 
+    fun copy(events: List<Event>): EventsOfDay = EventsOfDay(date, events, startsAtZero, id)
+
     companion object {
-        fun createEmpty(date: LocalDate): EventsOfDay = EventsOfDay(date, mutableListOf(), id = "")
+        fun createEmpty(date: LocalDate): EventsOfDay =
+            EventsOfDay(date, mutableListOf(), id = UUIDS.createShort())
     }
 }
