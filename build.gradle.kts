ae17840c465292d13c3a844d9b2513fc2f2cf15f
Denis <denchic150@gmail.com>
2022-09-17 13:17:27 +0300

    Update gradle tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
 
     dependencies {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
-        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("com.android.tools.build:gradle:7.3.0")
         classpath("com.google.gms:google-services:4.3.13")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.squareup.sqldelight:gradle-plugin:1.5.3")
