2731d937e8336aa7accf2ad32b92d29257f12b2b
Denis <denchic150@gmail.com>
2022-09-25 23:35:35 +0300

    Add new theme variant

--- desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/theme/Theme.kt
+++ desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/theme/Theme.kt
@@ -5,13 +5,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColorScheme(
 
 )
 
 private val LightColorPalette = lightColorScheme(
-
+    surface = Color(0xFFF2F7FF)
 )
 
 @Composable
