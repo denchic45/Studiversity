7bafcf59cb914fdc9b5224113a403193ef8eebde
Denis <denchic150@gmail.com>
2022-10-05 00:22:00 +0300

    Add preview function for testing composables in runtime

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -12,78 +12,58 @@ import androidx.compose.ui.window.application
 import com.denchic45.kts.data.db.local.DriverFactory
 import com.denchic45.kts.di.*
 import com.denchic45.kts.ui.MainContent
+import com.denchic45.kts.ui.group.members.ProfilePreview
 import com.denchic45.kts.ui.login.LoginScreen
-import com.denchic45.kts.ui.timetable.desktopConfig
-import io.kamel.image.config.LocalKamelConfig
+import com.denchic45.kts.ui.theme.AppTheme
 import java.awt.Toolkit
-import com.denchic45.kts.ui.theme2.AppTheme
 
-val appComponent = JvmAppComponent::class.create(
-    PreferencesComponent::class.create(SettingsFactory()),
-    DatabaseComponent::class.create(DriverFactory()),
-    NetworkComponent::class.create()
-)
+val appComponent =
+    JvmAppComponent::class.create(PreferencesComponent::class.create(SettingsFactory()),
+        DatabaseComponent::class.create(DriverFactory()),
+        NetworkComponent::class.create())
 
 val splashComponent = appComponent.splashComponent
 
-fun main() = application {
+fun main() = mainApp()
 
-    val isAuth by splashComponent.isAuth.collectAsState(null)
+private fun mainApp() {
+    application {
 
-    isAuth?.let {
-        if (it) {
-            val size = Toolkit.getDefaultToolkit().screenSize.run {
-                DpSize((width - 124).dp, (height - 124).dp)
-            }
+        val isAuth by splashComponent.isAuth.collectAsState(null)
 
-            Window(
-                onCloseRequest = ::exitApplication,
-                state = WindowState(size = size, position = WindowPosition(Alignment.Center))
-            ) {
-                AppTheme {
-                    CompositionLocalProvider(LocalKamelConfig provides desktopConfig) {
-                        MainContent(appComponent.rootComponent())
+        isAuth?.let {
+            if (it) {
+                val size = Toolkit.getDefaultToolkit().screenSize.run {
+                    DpSize((width - 124).dp, (height - 124).dp)
+                }
+
+                Window(onCloseRequest = ::exitApplication,
+                    state = WindowState(size = size, position = WindowPosition(Alignment.Center))) {
+                    AppTheme {
+                        CompositionLocalProvider {
+                            MainContent(appComponent.mainComponent())
+                        }
                     }
                 }
-            }
-        } else {
-            Window(onCloseRequest = ::exitApplication,
-                state = WindowState(size = DpSize(Dp.Unspecified, Dp.Unspecified))
-            ) {
-                LoginScreen(appComponent.loginComponent())
+            } else {
+                Window(onCloseRequest = ::exitApplication,
+                    state = WindowState(size = DpSize(Dp.Unspecified, Dp.Unspecified))) {
+                    LoginScreen(appComponent.loginComponent())
+                }
             }
         }
     }
 }
 
-//private fun testDatabase(users: List<String>) {
-//    var users1 = users
-//    GlobalScope.launch {
-//        delay(500)
-//        DriverFactory().driver.apply {
-//            DbHelper(this).database.apply {
-//                userEntityQueries.upsert(
-//                    UserEntity(
-//                        user_id = "1",
-//                        first_name = "Ivan",
-//                        surname = "Ivanoov",
-//                        patronymic = null,
-//                        user_group_id = "",
-//                        role = "STUDENT",
-//                        email = null,
-//                        photo_url = "",
-//                        gender = 1,
-//                        admin = false,
-//                        generated_avatar = true,
-//                        timestamp = 0
-//                    )
-//                )
-//                userEntityQueries
-//                    .getAll()
-//                    .asFlow()
-//                    .mapToList()
-//                    .collect { users1 = it.map { "User: ${it.first_name + it.surname}" } }
-//            }
-//        }
-//    }
-//}
+fun previewUi() {
+    application {
+        Window(onCloseRequest = ::exitApplication,
+            state = WindowState(size = DpSize(Dp.Unspecified, Dp.Unspecified))) {
+            AppTheme {
+                CompositionLocalProvider {
+                    ProfilePreview()
+                }
+            }
+        }
+    }
+}
