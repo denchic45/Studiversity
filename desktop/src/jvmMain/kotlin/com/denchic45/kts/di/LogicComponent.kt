9845fb5a47c1120907d860fe6a9e56ed5ff2dbc6
Denis <denchic150@gmail.com>
2022-10-05 00:23:08 +0300

    Change rootComponent to mainComponent

--- desktop/src/jvmMain/kotlin/com/denchic45/kts/di/LogicComponent.kt
+++ desktop/src/jvmMain/kotlin/com/denchic45/kts/di/LogicComponent.kt
@@ -1,17 +1,14 @@
 package com.denchic45.kts.di
 
 import com.denchic45.kts.ui.login.LoginComponent
-import com.denchic45.kts.ui.root.RootComponent
+import com.denchic45.kts.ui.MainComponent
 import com.denchic45.kts.ui.splash.SplashComponent
-import com.denchic45.kts.ui.timetable.TimetableComponent
 
 interface LogicComponent {
 
     val splashComponent: SplashComponent
 
-    val rootComponent: () -> RootComponent
+    val mainComponent: () -> MainComponent
 
     val loginComponent: () -> LoginComponent
-
-//    val timetableComponent: TimetableComponent
 }
