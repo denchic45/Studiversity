99d51d294dbb60ebc18f913eb8486619b486a9f1
Denis <denchic150@gmail.com>
2022-10-05 11:51:27 +0300

    Add new use case

--- common/src/commonJvmMain/kotlin/com/denchic45/kts/domain/usecase/FindGroupMembersUseCase.kt
+++ common/src/commonJvmMain/kotlin/com/denchic45/kts/domain/usecase/FindGroupMembersUseCase.kt
@@ -1,14 +1,12 @@
 package com.denchic45.kts.domain.usecase
 
-import com.denchic45.kts.domain.model.GroupMembers
 import com.denchic45.kts.data.repository.GroupRepository
+import com.denchic45.kts.domain.model.GroupMembers
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class FindGroupMembersUseCase @Inject constructor(
-    private val groupRepository: GroupRepository
-) {
-
+@me.tatarka.inject.annotations.Inject
+class FindGroupMembersUseCase @Inject constructor(private val groupRepository: GroupRepository) {
     operator fun invoke(groupId: String): Flow<GroupMembers> {
         return groupRepository.findGroupMembersByGroupId(groupId)
     }
