73ce0fa1eb961d23d5e4f959275023b7d07edc00
Denis <denchic150@gmail.com>
2022-10-06 11:13:33 +0300

    Add file with components from Material 2

---
+++ desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/theme/Compat.kt
@@ -0,0 +1,52 @@
+package com.denchic45.kts.ui.theme
+
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+
+@Composable
+fun Text2(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    style: TextStyle = LocalTextStyle.current
+) = androidx.compose.material.Text(
+    text,
+    modifier,
+    color,
+    fontSize,
+    fontStyle,
+    fontWeight,
+    fontFamily,
+    letterSpacing,
+    textDecoration,
+    textAlign,
+    lineHeight,
+    overflow,
+    softWrap,
+    maxLines,
+    onTextLayout,
+    style
+)
