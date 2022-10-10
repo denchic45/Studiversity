3a73df1cfe63d1f49cf98145db70156d50a4aa9e
Denis <denchic150@gmail.com>
2022-10-10 10:55:21 +0300

    Add InteractionSource

--- desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/components/UserListItem.kt
+++ desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/components/UserListItem.kt
@@ -3,7 +3,6 @@ package com.denchic45.kts.ui.components
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.CircleShape
@@ -13,7 +12,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,8 +26,10 @@ import com.denchic45.kts.util.loadImageBitmap
 
 @Composable
 fun HeaderItem(name: String) {
-    Row(Modifier.height(48.dp).padding(horizontal = 16.dp),
-        verticalAlignment = Alignment.CenterVertically) {
+    Row(
+        Modifier.height(48.dp).padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Text(name, style = MaterialTheme.typography.titleMedium)
     }
 }
@@ -39,38 +39,38 @@ fun UserListItem(
     modifier: Modifier = Modifier,
     onClick: (id: String) -> Unit,
     item: UserItem,
-    actionsOnHover: Boolean = false,
+    actionsVisible: Boolean = true,
     selected: Boolean = false,
+    interactionSource: MutableInteractionSource = remember(::MutableInteractionSource),
     actions: (@Composable () -> Unit)? = null,
 ) {
-    val interactionSource = remember(::MutableInteractionSource)
     item.apply {
         val shape = RoundedCornerShape(16.dp)
-        Row(modifier = modifier.run {
-            if (selected) background(MaterialTheme.colorScheme.secondaryContainer, shape)
-            else this
-        }.clip(shape).height(64.dp).selectable(onClick = { onClick(item.id) },
-            selected = selected,
-            interactionSource = interactionSource,
-            indication = rememberRipple(color = MaterialTheme.colorScheme.secondary))
-            .padding(horizontal = 16.dp), verticalAlignment = Alignment.CenterVertically) {
-            AsyncImage(load = { loadImageBitmap(photoUrl) },
+        Row(
+            modifier = modifier.run {
+                if (selected) background(MaterialTheme.colorScheme.secondaryContainer, shape)
+                else this
+            }.clip(shape).height(64.dp).selectable(
+                onClick = { onClick(item.id) },
+                selected = selected,
+                interactionSource = interactionSource,
+                indication = rememberRipple(color = MaterialTheme.colorScheme.secondary)
+            ).padding(horizontal = 16.dp), verticalAlignment = Alignment.CenterVertically
+        ) {
+            AsyncImage(
+                load = { loadImageBitmap(photoUrl) },
                 painterFor = { BitmapPainter(it) },
                 null,
-                Modifier.size(40.dp).clip(CircleShape)) { Box(Modifier.size(40.dp)) }
+                Modifier.size(40.dp).clip(CircleShape)
+            ) { Box(Modifier.size(40.dp)) }
             Column(Modifier.padding(start = 16.dp)) {
                 Text(title, style = MaterialTheme.typography.bodyLarge)
                 subtitle?.let { Text(it, style = MaterialTheme.typography.labelMedium) }
             }
             Spacer(Modifier.weight(1f))
             Row(Modifier.padding(vertical = 20.dp)) {
-                if (actions != null) {
-                    if (actionsOnHover) {
-                        val hovered by interactionSource.collectIsHoveredAsState()
-                        if (hovered) actions.invoke()
-                    } else {
-                        actions.invoke()
-                    }
+                if (actions != null && actionsVisible) {
+                    actions()
                 }
             }
         }
@@ -83,11 +83,12 @@ fun UserItemPreview() {
     val modifier = Modifier
     UserListItem(modifier,
         {},
-        UserItem("",
+        UserItem(
+            "",
             "User",
             "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Android_new_logo_2019.svg/2560px-Android_new_logo_2019.svg.png",
-            "Sub"),
-        actions = {
+            "Sub"
+        ), actions = {
             Icon(painterResource("drawable/ic_subject_history.xml"), null)
         })
 }
