e2755ad364eaed5cddb1a9ace74817370bf50aa7
Denis <denchic150@gmail.com>
2022-10-10 11:17:54 +0300

    Add kotlin-inject

--- common/src/commonJvmMain/kotlin/com/denchic45/kts/domain/usecase/RemoveHeadmanUseCase.kt
+++ common/src/commonJvmMain/kotlin/com/denchic45/kts/domain/usecase/RemoveHeadmanUseCase.kt
@@ -3,6 +3,7 @@ package com.denchic45.kts.domain.usecase
 import com.denchic45.kts.data.repository.GroupRepository
 import javax.inject.Inject
 
+@me.tatarka.inject.annotations.Inject
 class RemoveHeadmanUseCase @Inject constructor(private val groupRepository: GroupRepository) {
 
     suspend operator fun invoke(groupId: String) {
