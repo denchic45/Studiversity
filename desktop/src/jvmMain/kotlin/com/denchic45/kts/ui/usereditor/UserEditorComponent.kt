d2840ae0c82587b880575f6227e60d39f06ef0a4
Denis <denchic150@gmail.com>
2022-10-17 11:42:59 +0300

    Update user editor: - add state flows for fields - add config params

--- desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/usereditor/UserEditorComponent.kt
+++ desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/usereditor/UserEditorComponent.kt
@@ -1,21 +1,85 @@
 package com.denchic45.kts.ui.usereditor
 
+import com.denchic45.kts.data.domain.model.UserRole
 import com.denchic45.kts.ui.model.MenuAction
 import com.denchic45.kts.ui.model.MenuItem
+import com.denchic45.kts.util.randomAlphaNumericString
+import kotlinx.coroutines.flow.MutableStateFlow
+import me.tatarka.inject.annotations.Inject
 
-class UserEditorComponent {
+
+@Inject
+class UserEditorComponent(userId: String?, role: UserRole, groupId: String?) {
     val genders = listOf(MenuItem(GenderAction.Male), MenuItem(GenderAction.Female))
 
+    val firstNameField = MutableStateFlow("")
+
+    val surnameField = MutableStateFlow("")
+
+    val patronymicField = MutableStateFlow("")
+
+    val emailField = MutableStateFlow("")
+
+    val passwordField = MutableStateFlow("")
+
+    val genderField = MutableStateFlow(GenderAction.Undefined)
+
+    val roleField = MutableStateFlow(UserRole.TEACHER)
+
+    val groupField = MutableStateFlow("")
+
+    val photoUrl = MutableStateFlow("")
+
+    val groupFieldVisibility = MutableStateFlow(false)
+
+    val accountFieldsVisibility = MutableStateFlow(true)
+
+    init {
+
+    }
+
+    fun onFirstNameType(firstName: String) {
+        firstNameField.value = firstName
+    }
+
+    fun onSurnameType(surname: String) {
+        surnameField.value = surname
+    }
+
+    fun onPatronymicType(patronymic: String) {
+        patronymicField.value = patronymic
+    }
+
+    fun onEmailType(email: String) {
+        emailField.value = email
+    }
+
     fun onGenderSelect(action: GenderAction) {
-        when (action) {
-            GenderAction.Male -> TODO()
-            GenderAction.Female -> TODO()
-        }
+        genderField.value = action
+    }
+
+    fun onPasswordType(password: String) {
+        passwordField.value = password
+    }
+
+    fun onGroupClick() {
+//        viewModelScope.launch {
+//            navigateTo(MobileNavigationDirections.actionGlobalGroupChooserFragment())
+//            groupChooserInteractor.receiveSelectedGroup().let {
+//                groupId = it.id
+//                groupField.value = it.name
+//            }
+//        }
+    }
+
+    fun onPasswordGenerateClick() {
+        passwordField.value = randomAlphaNumericString(16)
     }
 }
 
 enum class GenderAction(override val title: String, override val iconName: String? = null) :
     MenuAction {
+    Undefined(""),
     Male("Мужской"),
     Female("Женский")
 }
--- desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/usereditor/UserEditorScreen.kt
+++ desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/usereditor/UserEditorScreen.kt
@@ -4,12 +4,20 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.IconButton
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Visibility
+import androidx.compose.material.icons.outlined.VisibilityOff
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.denchic45.kts.ui.components.HeaderItem
 import com.denchic45.kts.ui.components.Spinner
@@ -21,46 +29,76 @@ import com.denchic45.kts.util.loadImageBitmap
 @Composable
 fun UserEditorScreen(component: UserEditorComponent) {
     Column(Modifier.padding(vertical = 48.dp)) {
-        AsyncImage(load = { loadImageBitmap("") },
+        AsyncImage(
+            load = { loadImageBitmap("") },
             painterFor = { BitmapPainter(it) },
-            null) {
+            null
+        ) {
             Box(Modifier.size(40.dp))
         }
         HeaderItem("Личные данные")
+        val firstName by component.firstNameField.collectAsState()
         TextField(
-            value = "",
-            onValueChange = {},
-            label = { TextM2("Имя") }
+            value = firstName,
+            onValueChange = component::onFirstNameType,
+            label = { TextM2("Имя") },
+            singleLine = true
         )
+        val surname by component.surnameField.collectAsState()
         TextField(
-            value = "",
-            onValueChange = {},
+            value = surname,
+            onValueChange = component::onSurnameType,
             Modifier.padding(top = 24.dp),
-            label = { TextM2("Фамилия") }
+            label = { TextM2("Фамилия") },
+            singleLine = true
         )
+        val patronymic by component.patronymicField.collectAsState()
         TextField(
-            value = "",
-            onValueChange = {},
+            value = patronymic,
+            onValueChange = component::onPatronymicType,
             Modifier.padding(top = 24.dp),
             label = { TextM2("Отчество") },
-            placeholder = { TextM2("Отчество (необязательно)") }
+            placeholder = { TextM2("Отчество (необязательно)") },
+            singleLine = true
         )
 
+        val gender by component.genderField.collectAsState()
         var expanded by remember { mutableStateOf(false) }
         Spinner(
             items = component.genders,
-            onActionClick = { component.onGenderSelect(it) },
+            onActionClick = component::onGenderSelect,
             expanded = expanded,
             onExpandedChange = { expanded = it },
-            placeholder = "Пол"
+            placeholder = "Пол",
+            activeAction = gender
         )
         HeaderItem("Вход в аккаунт")
+        val email by component.emailField.collectAsState()
         TextField(
-            value = "",
-            onValueChange = {},
+            value = email,
+            onValueChange = component::onEmailType,
             Modifier.padding(top = 24.dp),
-            label = { TextM2("Фамилия") },
+            label = { TextM2("Почта") },
             leadingIcon = { Icon(painterResource("ic_email".toDrawablePath()), null) }
+        )
+        val password by component.passwordField.collectAsState()
+        var passwordVisible by remember { mutableStateOf(false) }
+        TextField(
+            value = password,
+            onValueChange = component::onPasswordType,
+            Modifier.padding(top = 24.dp),
+            label = { TextM2("Пароль") },
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    Icon(
+                        imageVector = if (passwordVisible) Icons.Outlined.Visibility else Icons.Outlined.VisibilityOff,
+                        if (passwordVisible) "Hide password" else "Show password"
+                    )
+                }
+            },
+            singleLine = true
         )
     }
 }
