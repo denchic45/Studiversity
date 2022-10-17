602c8c6153514c5c75c4c0d53c5e5b469fdff1f2
Denis <denchic150@gmail.com>
2022-10-17 23:27:03 +0300

    Make params nullable

--- desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/navigation/UserEditor.kt
+++ desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/navigation/UserEditor.kt
@@ -3,6 +3,10 @@ package com.denchic45.kts.ui.navigation
 import com.denchic45.kts.data.domain.model.UserRole
 import com.denchic45.kts.ui.usereditor.UserEditorComponent
 
-class UserEditorConfig( val userId: String, val  role: UserRole, val  groupId: String) : GroupMembersConfig
+class UserEditorConfig(
+    val userId: String?,
+    val role: UserRole?,
+    val groupId: String?,
+) : GroupMembersConfig
 
-class UserEditorChild(userEditorComponent: UserEditorComponent) : GroupMembersChild
+class UserEditorChild(val userEditorComponent: UserEditorComponent) : GroupMembersChild
