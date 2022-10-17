ee3725abd1a97cd15db3c081340c823463a0b201
Denis <denchic150@gmail.com>
2022-10-17 11:34:35 +0300

    Add fun for generate password

--- common/src/commonJvmMain/kotlin/com/denchic45/kts/util/UUIDS.kt
+++ common/src/commonJvmMain/kotlin/com/denchic45/kts/util/UUIDS.kt
@@ -5,3 +5,11 @@ import java.util.*
 object UUIDS {
     fun createShort(): String = UUID.randomUUID().toString().substring(0, 13)
 }
+
+fun randomAlphaNumericString(desiredStrLength: Int): String {
+    val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    return (1..desiredStrLength)
+        .map { kotlin.random.Random.nextInt(0, charPool.size) }
+        .map(charPool::get)
+        .joinToString("")
+}
