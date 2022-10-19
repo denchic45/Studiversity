0ad1c59a85923f2bb17057085df2b8f5c630be2e
Denis <denchic150@gmail.com>
2022-10-19 11:43:50 +0300

    Update UI

--- desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/usereditor/UserEditorScreen.kt
+++ desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/usereditor/UserEditorScreen.kt
@@ -1,17 +1,25 @@
 package com.denchic45.kts.ui.usereditor
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.IconButton
+import androidx.compose.material.*
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.Link
 import androidx.compose.material.icons.outlined.Visibility
 import androidx.compose.material.icons.outlined.VisibilityOff
 import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,7 +43,7 @@ import com.denchic45.kts.util.loadImageBitmap
 fun UserEditorScreen(component: UserEditorComponent, modifier: Modifier = Modifier) {
     Column(modifier) {
         SmallTopAppBar(
-            title = { Text("Новый студент") },
+            title = { Text(component.toolbarTitle) },
             navigationIcon = {
                 IconButton(onClick = component::onSaveClick) {
                     Icon(
@@ -94,45 +102,93 @@ fun UserEditorScreen(component: UserEditorComponent, modifier: Modifier = Modifi
                 singleLine = true
             )
             val gender by component.genderField.collectAsState()
-            var expanded by remember { mutableStateOf(false) }
+            var expandedGenders by remember { mutableStateOf(false) }
             Spinner(
                 items = component.genders,
                 onActionClick = component::onGenderSelect,
                 modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 24.dp).fillMaxWidth(),
-                expanded = expanded,
-                onExpandedChange = { expanded = it },
+                expanded = expandedGenders,
+                onExpandedChange = { expandedGenders = it },
                 placeholder = "Пол",
                 activeAction = gender
             )
-            HeaderItem("Вход в аккаунт")
-            val email by component.emailField.collectAsState()
-            OutlinedTextField(
-                value = email,
-                onValueChange = component::onEmailType,
-                Modifier.padding(horizontal = 16.dp).fillMaxWidth(),
-                label = { TextM2("Почта") },
-                leadingIcon = { Icon(painterResource("ic_email".toDrawablePath()), null) },
-                singleLine = true
-            )
-            val password by component.passwordField.collectAsState()
-            var passwordVisible by remember { mutableStateOf(false) }
-            OutlinedTextField(
-                value = password,
-                onValueChange = component::onPasswordType,
-                Modifier.padding(start = 16.dp, end = 16.dp, top = 24.dp).fillMaxWidth(),
-                label = { TextM2("Пароль") },
-                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                trailingIcon = {
-                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
-                        Icon(
-                            imageVector = if (passwordVisible) Icons.Outlined.Visibility else Icons.Outlined.VisibilityOff,
-                            if (passwordVisible) "Hide password" else "Show password"
-                        )
-                    }
-                },
-                singleLine = true
-            )
+            val availableRoles by component.availableRoles.collectAsState()
+            val role by component.roleField.collectAsState()
+            availableRoles?.let { roles ->
+                var expandedRoles by remember { mutableStateOf(false) }
+                Spinner(
+                    items = roles,
+                    onActionClick = component::onRoleSelect,
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 24.dp)
+                        .fillMaxWidth(),
+                    expanded = expandedRoles,
+                    onExpandedChange = { expandedRoles = it },
+                    placeholder = "Роль",
+                    activeAction = role
+                )
+            }
+
+            val accountFieldsVisibility by component.accountFieldsVisibility.collectAsState()
+            if (accountFieldsVisibility) {
+                HeaderItem("Вход в аккаунт")
+                val email by component.emailField.collectAsState()
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = component::onEmailType,
+                    Modifier.padding(horizontal = 16.dp).fillMaxWidth(),
+                    label = { TextM2("Почта") },
+                    leadingIcon = { Icon(painterResource("ic_email".toDrawablePath()), null) },
+                    singleLine = true
+                )
+                val password by component.passwordField.collectAsState()
+                var passwordVisible by remember { mutableStateOf(false) }
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = component::onPasswordType,
+                    Modifier.padding(start = 16.dp, end = 16.dp, top = 24.dp).fillMaxWidth(),
+                    label = { TextM2("Пароль") },
+                    visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    trailingIcon = {
+                        IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                            Icon(
+                                imageVector = if (passwordVisible) Icons.Outlined.Visibility else Icons.Outlined.VisibilityOff,
+                                if (passwordVisible) "Hide password" else "Show password"
+                            )
+                        }
+                    },
+                    singleLine = true
+                )
+            }
+            val groupVisibility by component.groupFieldVisibility.collectAsState()
+            if (groupVisibility) {
+                val groupHeader by component.groupField.collectAsState()
+                androidx.compose.material.Divider(
+                    Modifier.padding(
+                        start = 16.dp,
+                        end = 16.dp,
+                        top = 24.dp
+                    )
+                )
+                OutlinedTextField(
+                    value = groupHeader.name,
+                    onValueChange = component::onPasswordType,
+                    Modifier.padding(start = 16.dp, end = 16.dp, top = 24.dp).fillMaxWidth()
+                        .clickable(onClick = component::onGroupClick),
+                    label = { TextM2("Группа") },
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        disabledTextColor = LocalContentColor.current.copy(LocalContentAlpha.current),
+                        disabledLabelColor = MaterialTheme.colorScheme.onSurface.copy(ContentAlpha.medium)
+                    ),
+                    trailingIcon = {
+                        IconButton(onClick = component::onGroupClick) {
+                            Icon(imageVector = Icons.Outlined.Link, "Select group")
+                        }
+                    },
+                    enabled = false,
+                    singleLine = true
+                )
+            }
             Spacer(Modifier.height(48.dp))
         }
     }
