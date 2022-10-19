e38dec5c393252c73dde3dcc19329cb2a4ee4e05
Denis <denchic150@gmail.com>
2022-10-19 11:44:22 +0300

    Change conditions generic variance

--- common/src/commonMain/kotlin/com/denchic45/uivalidator/experimental/Validator.kt
+++ common/src/commonMain/kotlin/com/denchic45/uivalidator/experimental/Validator.kt
@@ -2,7 +2,7 @@ package com.denchic45.uivalidator.experimental
 
 
 class Validator<T>(
-    private val conditions: List<Condition<T>>,
+    private val conditions: List<Condition<out T>>,
     val operator: Operator = Operator.AllEach,
     override val onResult: ValidationResult? = null,
 ) : Condition<T> {
