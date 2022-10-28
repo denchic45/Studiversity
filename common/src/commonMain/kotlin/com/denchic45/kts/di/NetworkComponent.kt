7e85e2393db7f0eab16ffe2369a581aeabe079c7
Denis <denchic150@gmail.com>
2022-10-28 15:59:05 +0300

    Get avatar with correct size and random background color

--- common/src/commonMain/kotlin/com/denchic45/kts/di/NetworkComponent.kt
+++ common/src/commonMain/kotlin/com/denchic45/kts/di/NetworkComponent.kt
@@ -1,8 +1,8 @@
 package com.denchic45.kts.di
 
 import com.denchic45.kts.ApiKeys
-import com.denchic45.kts.data.service.model.RefreshTokenResponse
 import com.denchic45.kts.data.pref.AppPreferences
+import com.denchic45.kts.data.service.model.RefreshTokenResponse
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.plugins.auth.*
