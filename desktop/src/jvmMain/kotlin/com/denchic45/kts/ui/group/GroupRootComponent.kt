475fe9e275331aac9e8177e58eb925a5708c8e78
Denis <denchic150@gmail.com>
2022-12-10 15:51:10 +0300

    Add overlay navigation

--- desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/group/GroupRootComponent.kt
+++ desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/group/GroupRootComponent.kt
@@ -1,12 +1,16 @@
 package com.denchic45.kts.ui.group
 
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.overlay.*
 import com.arkivanov.decompose.router.stack.StackNavigation
 import com.arkivanov.decompose.router.stack.StackNavigator
 import com.arkivanov.decompose.router.stack.childStack
+import com.arkivanov.decompose.router.stack.pop
+import com.arkivanov.decompose.value.Value
+import com.denchic45.kts.data.domain.model.UserRole
 import com.denchic45.kts.data.pref.GroupPreferences
-import com.denchic45.kts.ui.navigation.GroupChild
-import com.denchic45.kts.ui.navigation.GroupConfig
+import com.denchic45.kts.ui.navigation.*
+import com.denchic45.kts.ui.usereditor.UserEditorComponent
 import me.tatarka.inject.annotations.Inject
 
 
@@ -15,6 +19,7 @@ class GroupRootComponent(
     private val lazyGroupComponent: (navigator: StackNavigator<in GroupConfig>, groupId: String) -> GroupComponent,
     componentContext: ComponentContext,
     groupPreferences: GroupPreferences,
+    userEditorComponent: (onFinish: () -> Unit, config: UserEditorConfig) -> UserEditorComponent,
 ) : ComponentContext by componentContext {
 
     private val groupId: String = groupPreferences.groupId
@@ -29,4 +34,27 @@ class GroupRootComponent(
                 is GroupConfig -> GroupChild.Group(groupComponent)
             }
         })
+
+    private val overlayNavigation = OverlayNavigation<GroupOverlayConfig>()
+    val childOverlay: Value<ChildOverlay<GroupOverlayConfig, GroupOverlayChild>> = childOverlay(
+        source = overlayNavigation,
+        handleBackButton = true
+    ) { config, _ ->
+        when (config) {
+            is UserEditorConfig -> UserEditorChild(
+                userEditorComponent(
+                    navigation::pop,
+                    config
+                )
+            )
+        }
+    }
+
+    fun onAddStudentClick() {
+        overlayNavigation.activate(UserEditorConfig(null, UserRole.STUDENT, groupId))
+    }
+
+    fun onDialogDismiss() {
+        overlayNavigation.dismiss()
+    }
 }
