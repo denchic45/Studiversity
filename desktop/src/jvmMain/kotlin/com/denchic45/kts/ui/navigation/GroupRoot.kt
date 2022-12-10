1c4b0c1732ffe05c29facc3ad678d892dbf4e146
Denis <denchic150@gmail.com>
2022-12-10 15:51:10 +0300

    Add overlay navigation

--- desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/navigation/GroupRoot.kt
+++ desktop/src/jvmMain/kotlin/com/denchic45/kts/ui/navigation/GroupRoot.kt
@@ -10,7 +10,3 @@ sealed interface GroupConfig : Parcelable {
 sealed interface GroupChild {
     class Group(val groupComponent: GroupComponent) : GroupChild
 }
-
-sealed interface GroupOverlayConfig : Parcelable
-
-sealed interface GroupOverlayChild
