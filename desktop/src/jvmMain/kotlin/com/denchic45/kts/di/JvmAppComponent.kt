d8a6a2c91c40238748a76d910cccf897a217612e
Denis <denchic150@gmail.com>
2022-12-10 15:51:10 +0300

    Change scope

--- desktop/src/jvmMain/kotlin/com/denchic45/kts/di/JvmAppComponent.kt
+++ desktop/src/jvmMain/kotlin/com/denchic45/kts/di/JvmAppComponent.kt
@@ -2,20 +2,22 @@ package com.denchic45.kts.di
 
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.decompose.router.overlay.OverlayNavigation
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.denchic45.kts.data.service.AppVersionService
 import com.denchic45.kts.data.service.FakeAppVersionService
+import com.denchic45.kts.ui.navigation.OverlayConfig
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Provides
-import javax.inject.Singleton
 
+@AppScope
 @Component
 abstract class JvmAppComponent(
     @Component val preferencesComponent: PreferencesComponent,
     @Component val databaseComponent: DatabaseComponent,
-    @Component val networkComponent: NetworkComponent,
+    @Component val networkComponent: NetworkComponent
 ) : LogicComponent {
 
     @Provides
@@ -23,10 +25,17 @@ abstract class JvmAppComponent(
         return DefaultComponentContext(LifecycleRegistry())
     }
 
+    @AppScope
     @Provides
-    @Singleton
     fun provideApplicationScope() = CoroutineScope(SupervisorJob())
 
+    @AppScope
+    @Provides
+    fun provideOverlayNavigator(): OverlayNavigation<OverlayConfig> {
+        return OverlayNavigation()
+    }
+
+    @AppScope
     @Provides
     fun provideAppVersionService(coroutineScope: CoroutineScope): AppVersionService {
         return FakeAppVersionService(coroutineScope)
