d6e5e906a3322cd290941ca665001400986af2dc
Denis <denchic150@gmail.com>
2023-01-25 23:38:10 +0300

    Update gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
     plugins {
         id("org.jetbrains.kotlin.android") version "1.7.20"
         id("org.jetbrains.kotlin.jvm") version "1.7.20"
-        id("com.android.library") version "7.3.1"
+        id("com.android.library") version "7.4.0"
     }
 }
 
