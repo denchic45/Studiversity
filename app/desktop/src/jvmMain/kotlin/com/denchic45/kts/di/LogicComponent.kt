f79a775b60ef7bab1b444bfb8ca55871719ba4c8
Denis <denchic150@gmail.com>
2023-04-02 21:37:51 +0300

    Remove useless files

--- app/desktop/src/jvmMain/kotlin/com/denchic45/kts/di/LogicComponent.kt
+++
@@ -1,9 +0,0 @@
-package com.denchic45.kts.di
-
-import com.denchic45.kts.ui.MainComponent
-import com.denchic45.kts.ui.login.LoginComponent
-import com.denchic45.kts.ui.splash.SplashComponent
-
-interface LogicComponent {
-
-}
