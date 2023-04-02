fd0246fd170a28718cc7d7fad3fbaac5d1ba6ba0
Denis <denchic150@gmail.com>
2023-04-02 21:33:32 +0300

    Add a useful fun instead of others

--- app/common/src/commonMain/kotlin/com/denchic45/kts/util/Collections.kt
+++ app/common/src/commonMain/kotlin/com/denchic45/kts/util/Collections.kt
@@ -1,7 +1,6 @@
 package com.denchic45.kts.util
 
 
-fun <T> Iterable<T>.updated(old: T, new: T): List<T> = map { if (it == old) new else it }
-
-fun <T> Iterable<T>.updated(index: Int, new: T): List<T> =
-    mapIndexed { i, e -> if (i == index) new else e }
+inline fun <T> List<T>.copy(mutatorBlock: MutableList<T>.() -> Unit): List<T> {
+    return toMutableList().apply(mutatorBlock)
+}
