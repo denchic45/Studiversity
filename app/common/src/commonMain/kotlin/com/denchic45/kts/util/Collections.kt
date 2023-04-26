99419c2e4851116b7fd8c84d14c04d0ebeaed502
Denis <denchic150@gmail.com>
2023-04-26 11:45:30 +0300

    Update utils

--- app/common/src/commonMain/kotlin/com/denchic45/kts/util/Collections.kt
+++
@@ -1,6 +0,0 @@
-package com.denchic45.kts.util
-
-
-inline fun <T> List<T>.copy(mutatorBlock: MutableList<T>.() -> Unit): List<T> {
-    return toMutableList().apply(mutatorBlock)
-}
--- app/common/src/commonMain/kotlin/com/denchic45/kts/util/Lists.kt
+++ app/common/src/commonMain/kotlin/com/denchic45/kts/util/Lists.kt
@@ -1,9 +1,15 @@
 package com.denchic45.kts.util
 
 fun <T> List<T>.swap(index1: Int, index2: Int): List<T> {
-    return toMutableList().apply {
-        val tmp = this[index1] // 'this' corresponds to the list
-        this[index1] = this[index2]
-        this[index2] = tmp
-    }
+    return toMutableList().apply { swap(index1, index2) }
+}
+
+fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
+    val tmp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = tmp
+}
+
+inline fun <T> List<T>.copy(mutatorBlock: MutableList<T>.() -> Unit): List<T> {
+    return toMutableList().apply(mutatorBlock)
 }
