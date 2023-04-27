fd6722180fab0f5514c67d8e1be1a5ff3c5b0434
Denis <denchic150@gmail.com>
2023-04-27 11:26:19 +0300

    Update utils

--- app/common/src/androidMain/kotlin/com/denchic45/kts/util/FlowLifecycle.kt
+++ app/common/src/androidMain/kotlin/com/denchic45/kts/util/FlowLifecycle.kt
@@ -1,7 +1,11 @@
 package com.denchic45.kts.util
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.Flow
@@ -34,3 +38,16 @@ fun <T> Flow<T>.collectWhenResumed(
     lifecycleOwner: LifecycleOwner,
     action: FlowCollector<T> = FlowCollector {}
 ) = collectWhen(lifecycleOwner, Lifecycle.State.RESUMED, action)
+
+@Composable
+inline fun <reified T> Flow<T>.collectWithLifecycle(
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+    noinline action: suspend (T) -> Unit
+) {
+    LaunchedEffect(key1 = Unit) {
+        lifecycleOwner.lifecycleScope.launch {
+            flowWithLifecycle(lifecycleOwner.lifecycle, minActiveState).collect(action)
+        }
+    }
+}
