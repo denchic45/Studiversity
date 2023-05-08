6a4d6f6ff92ba392f7a23dabf4ae2fe044af70e3
Denis <denchic150@gmail.com>
2023-05-08 23:31:54 +0300

    Update search subjects

--- studiversity-backend/src/main/kotlin/com/studiversity/feature/course/subject/SubjectRepository.kt
+++ studiversity-backend/src/main/kotlin/com/studiversity/feature/course/subject/SubjectRepository.kt
@@ -1,17 +1,15 @@
 package com.studiversity.feature.course.subject
 
+import com.denchic45.stuiversity.api.course.subject.model.CreateSubjectRequest
+import com.denchic45.stuiversity.api.course.subject.model.SubjectResponse
+import com.denchic45.stuiversity.api.course.subject.model.UpdateSubjectRequest
 import com.studiversity.database.table.CourseDao
 import com.studiversity.database.table.Courses
 import com.studiversity.database.table.SubjectDao
 import com.studiversity.database.table.Subjects
-import com.denchic45.stuiversity.api.course.subject.model.CreateSubjectRequest
-import com.denchic45.stuiversity.api.course.subject.model.UpdateSubjectRequest
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.like
-import org.jetbrains.exposed.sql.lowerCase
-import org.jetbrains.exposed.sql.or
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.jetbrains.exposed.sql.trim
 import java.util.*
 
 class SubjectRepository {
@@ -31,10 +29,16 @@ class SubjectRepository {
         SubjectDao.all().toResponses()
     }
 
-    fun find(query: String) = SubjectDao.find(
-        Subjects.name.lowerCase().trim() like "%$query%"
-                or (Subjects.shortname.lowerCase().trim() like "%$query%")
-    ).map(SubjectDao::toResponse)
+    fun find(q: String?): List<SubjectResponse> {
+        val query = Subjects.selectAll()
+        q?.let {
+            query.andWhere {
+                Subjects.name.lowerCase().trim() like "%$query%" or
+                        (Subjects.shortname.lowerCase().trim() like "%$query%")
+            }
+        }
+        return SubjectDao.wrapRows(query).map(SubjectDao::toResponse)
+    }
 
     fun update(id: UUID, request: UpdateSubjectRequest) = transaction {
         SubjectDao.findById(id)?.apply {
--- studiversity-backend/src/main/kotlin/com/studiversity/feature/course/subject/usecase/SearchSubjectsUseCase.kt
+++ studiversity-backend/src/main/kotlin/com/studiversity/feature/course/subject/usecase/SearchSubjectsUseCase.kt
@@ -1,15 +1,15 @@
 package com.studiversity.feature.course.subject.usecase
 
+import com.denchic45.stuiversity.api.course.subject.model.SubjectResponse
 import com.studiversity.feature.course.subject.SubjectRepository
 import com.studiversity.transaction.TransactionWorker
 import com.studiversity.util.searchable
-import com.denchic45.stuiversity.api.course.subject.model.SubjectResponse
 
 class SearchSubjectsUseCase(
     private val transactionWorker: TransactionWorker,
     private val subjectRepository: SubjectRepository
 ) {
     operator fun invoke(query: String?): List<SubjectResponse> = transactionWorker {
-        query?.let { subjectRepository.find(it.searchable()) } ?: subjectRepository.findAll()
+        subjectRepository.find(query?.searchable())
     }
 }
