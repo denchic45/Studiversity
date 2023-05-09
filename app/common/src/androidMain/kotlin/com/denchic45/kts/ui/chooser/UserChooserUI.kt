75622eec72f9cb537e094fd66bd770630db6183d
Denis <denchic150@gmail.com>
2023-05-09 18:18:31 +0300

    Fix user avatar

--- app/common/src/androidMain/kotlin/com/denchic45/kts/ui/chooser/UserChooserUI.kt
+++ app/common/src/androidMain/kotlin/com/denchic45/kts/ui/chooser/UserChooserUI.kt
@@ -1,8 +1,13 @@
 package com.denchic45.kts.ui.chooser
 
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 
 @Composable
@@ -11,7 +16,13 @@ fun UserChooserScreen(component: UserChooserComponent) {
         ListItem(
             headlineContent = { Text(it.fullName) },
             leadingContent = {
-                AsyncImage(model = it.avatarUrl, contentDescription = "user avatar")
+                AsyncImage(
+                    model = it.avatarUrl,
+                    contentDescription = "user avatar",
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(CircleShape),
+                )
             }
         )
     })
