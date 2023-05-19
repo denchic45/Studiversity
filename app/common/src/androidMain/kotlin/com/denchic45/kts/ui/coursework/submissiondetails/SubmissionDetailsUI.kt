1b7eb5fc626aaa052bf588fe387845218f278a03
Denis <denchic150@gmail.com>
2023-05-19 15:43:48 +0300

    Add illustration for empty attachments

--- app/common/src/androidMain/kotlin/com/denchic45/kts/ui/coursework/submissiondetails/SubmissionDetailsUI.kt
+++ app/common/src/androidMain/kotlin/com/denchic45/kts/ui/coursework/submissiondetails/SubmissionDetailsUI.kt
@@ -1,8 +1,8 @@
 package com.denchic45.kts.ui.coursework.submissiondetails
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Attachment
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
@@ -24,9 +26,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
 import com.denchic45.kts.domain.onSuccess
 import com.denchic45.kts.ui.attachment.AttachmentListItem
@@ -70,13 +75,10 @@ fun SubmissionDetailsScreen(
             onDismissRequest = onDismissRequest,
             sheetState = sheetState
         ) {
-//            SideEffect {
-//                coroutineScope.launch { sheetState.show() }
-//            }
             SubmissionHeaderContent(uiState)
             SubmissionDetailsContent(uiState, component::onAttachmentClick)
             Spacer(Modifier.height(MaterialTheme.spacing.normal))
-            Row(Modifier.padding(MaterialTheme.spacing.normal)) {
+            Column(Modifier.padding(MaterialTheme.spacing.normal)) {
                 uiState.grade?.let {
                     Text(
                         text = it.value.toString(),
@@ -97,7 +99,7 @@ fun SubmissionDetailsScreen(
                         modifier = Modifier.weight(1f),
                         keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
                     )
-                    Spacer(modifier = Modifier.width(MaterialTheme.spacing.normal))
+                    Spacer(modifier = Modifier.height(MaterialTheme.spacing.normal))
                     FilledTonalButton(
                         onClick = { component.onGrade(typedGrade.toInt()) },
                         enabled = typedGrade.isNotEmpty()
@@ -105,7 +107,6 @@ fun SubmissionDetailsScreen(
                         Text("Оценить")
                     }
                 }
-
             }
         }
     }
@@ -118,8 +119,8 @@ fun SubmissionDetailsContent(
     onAttachmentRemove: ((attachmentId: UUID) -> Unit)? = null
 ) {
     Column {
+        HeaderItemUI(name = "Прикрепленные файлы")
         if (uiState.attachments.isNotEmpty()) {
-            HeaderItemUI(name = "Прикрепленные файлы")
             LazyRow(Modifier) {
                 items(uiState.attachments, key = { it.attachmentId?.toString() ?: "" }) { item ->
                     Spacer(Modifier.width(MaterialTheme.spacing.normal))
@@ -127,6 +128,15 @@ fun SubmissionDetailsContent(
                         onAttachmentRemove?.let { it(item.attachmentId!!) }
                     }
                 }
+            }
+        } else {
+            Column(Modifier.height(96.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = rememberVectorPainter(image = Icons.Outlined.Attachment),
+                    contentDescription = "empty attachments"
+                )
+                Spacer(Modifier.height(MaterialTheme.spacing.normal))
+                Text("Нет прикрепленных файлов")
             }
         }
     }
