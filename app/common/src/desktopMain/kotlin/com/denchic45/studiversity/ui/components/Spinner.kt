abb9f16761060ef7d8a6598a23e26f6540703b28
Denis <denchic150@gmail.com>
2023-07-24 04:48:18 -0400

    Remove unnecessary files

--- app/common/src/desktopMain/kotlin/com/denchic45/studiversity/ui/components/Spinner.kt
+++ app/common/src/desktopMain/kotlin/com/denchic45/studiversity/ui/components/Spinner.kt
@@ -4,39 +4,33 @@ package com.denchic45.studiversity.ui.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.material.LocalContentAlpha
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import com.denchic45.studiversity.ui.model.MenuAction
-import com.denchic45.studiversity.ui.model.MenuItem
 import com.denchic45.studiversity.ui.theme.toDrawablePath
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun <T : MenuAction> Spinner(
-    items: List<MenuItem<T>>,
-    onActionClick: (action: T) -> Unit,
+fun Spinner2(
+    text: String,
     modifier: Modifier = Modifier,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
     expanded: Boolean,
     onExpandedChange: (Boolean) -> Unit,
-    placeholder: String? = null,
-    activeAction: T?,
+    itemsContent: @Composable ColumnScope.() -> Unit,
 ) {
-    var selectedItem by remember { mutableStateOf(items.find { it.action == activeAction }) }
 
     Column {
         OutlinedTextField(
-            value = selectedItem?.title ?: "",
+            value = text,
             onValueChange = {},
-            modifier.clickable { onExpandedChange(!expanded) },
-            label = { placeholder?.let { Text(it) } },
+            modifier = modifier.clickable { onExpandedChange(!expanded) },
+            placeholder = placeholder,
+            label = label,
             colors = TextFieldDefaults.outlinedTextFieldColors(
                 disabledTextColor = LocalContentColor.current.copy(LocalContentAlpha.current),
                 disabledLabelColor = LocalContentColor.current.copy(LocalContentAlpha.current),
@@ -53,26 +47,11 @@ fun <T : MenuAction> Spinner(
             singleLine = true
         )
 
-        DropdownMenu(expanded = expanded,
-            modifier = Modifier.width(240.dp),
+        DropdownMenu(
+            expanded = expanded,
             onDismissRequest = { onExpandedChange(false) }
         ) {
-            items.forEach { item ->
-                DropdownMenuItem(onClick = {
-                    onActionClick(item.action)
-                    selectedItem = item
-                }, enabled = item.enabled,
-                text = {
-                        item.action.iconName?.let {
-                            Icon(
-                                painter = painterResource(it.toDrawablePath()),
-                                null,
-                                Modifier.padding(end = 12.dp)
-                            )
-                        }
-                        Text(text = item.title, style = MaterialTheme.typography.bodyMedium)
-                })
-            }
+            itemsContent()
         }
     }
 }
