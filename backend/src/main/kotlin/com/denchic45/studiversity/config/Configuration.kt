c44cdd425b674df8ec0e8fb80c67872f5a8f1a66
denchic45 <denchic150@gmail.com>
2024-02-18 23:10:04 +0300

    Update config

--- backend/src/main/kotlin/com/denchic45/studiversity/config/Configuration.kt
+++ backend/src/main/kotlin/com/denchic45/studiversity/config/Configuration.kt
@@ -7,45 +7,40 @@ import kotlin.reflect.KProperty
 
 val dataFile: File = File("").absoluteFile
 
-//val configFile = dataFile.resolve("application.conf")
 val configFile = dataFile.resolve("config.properties")
-    .apply { if (!exists()) createNewFile() }
+    .apply { if (!exists()) {
+        createNewFile()
+        writeText("""
+            jwtSecret=${UUID.randomUUID()}
+            jwtRealm=sub
+            jwtAudience=authenticated
+        """.trimIndent())
+    } }
 
 val config by lazy { Configuration() }
 
 class Configuration {
     private val properties = Properties().apply { load(configFile.inputStream()) }
+    private fun <T> property(key: String? = null, defValue: T? = null) =
+        DefaultConfigPropertyDelegate(properties, key, defValue)
 
-    //    private val preferences = FilePreferences(null, "config")
-    private fun <T> property(key: String, defValue: T? = null) = ConfigPropertyDelegate(properties, key, defValue)
+    private fun <T : Any> property(key: String? = null, defValue: String? = null, map: (String) -> T) =
+        MappedConfigPropertyDelegate(DefaultConfigPropertyDelegate(properties, key, defValue), map)
 
-//    var dbUrl: String
-//        get() = preferences.get("db_url", "")
-//        set(value) = preferences.put("db_url", value)
+    var jwtAudience: String by property()
+    var jwtSecret: String by property()
+    var jwtRealm: String by property()
 
-//    var dbUser: String
-//        get() = preferences.get("db_user", "")
-//        set(value) = preferences.put("db_user", value)
+    var dbUrl: String by property()
+    var dbUser: String by property()
+    var dbPassword: String by property()
 
-//    var dbPassword: String
-//        get() = preferences.get("db_password", "")
-//        set(value) = preferences.put("db_password", value)
+    var organizationId: UUID by property(map = UUID::fromString)
+    var organizationName: String by property()
 
-    var jwtAudience: String by property("")
-    var jwtSecret: String by property("")
-    var jwtRealm: String by property("")
+    var selfRegister: Boolean by property()
 
-    var dbUrl: String by property("db_url")
-//    var dbName: String by property("db_name")
-    var dbUser: String by property("db_user")
-    var dbPassword: String by property("db_password")
-
-    var organizationId: UUID by property("organization_id")
-    var organizationName: String by property("organization_name")
-
-    var selfRegister: Boolean by property("self_register")
-
-    var initialized: Boolean by property("initialized", false)
+    var initialized: Boolean by property(defValue = false)
 }
 
 fun Configuration.database(url: String, user: String, password: String) {
@@ -54,16 +49,30 @@ fun Configuration.database(url: String, user: String, password: String) {
     dbPassword = password
 }
 
-private class ConfigPropertyDelegate<T>(
-    private val properties: Properties,
-    private val name: String,
-    private val defValue: T?
+private class MappedConfigPropertyDelegate<T>(
+    private val delegate: DefaultConfigPropertyDelegate<String>,
+    private val map: (String) -> T
 ) {
     operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
-        return properties.getOrDefault(name, defValue) as T
+        return map(delegate.getValue(thisRef, property))
     }
 
     operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
-        properties[name] = value
+        delegate.setValue(thisRef, property, value.toString())
+    }
+}
+
+private class DefaultConfigPropertyDelegate<T>(
+    private val properties: Properties,
+    private val key: String?,
+    private val defValue: T?
+) {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return properties.getOrDefault(key ?: property.name, defValue) as T
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        properties.setProperty(key ?: property.name, value.toString())
+        properties.store(configFile.outputStream(), null)
     }
 }
