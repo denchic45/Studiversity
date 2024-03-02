58be67e4bb506b69f24012caffc3c035ba79db82
denchic45 <denchic150@gmail.com>
2024-03-02 14:40:04 +0300

    Refactor code

--- app/common/src/androidMain/kotlin/com/denchic45/studiversity/ui/yourworks/YourWorksUI.kt
+++ app/common/src/androidMain/kotlin/com/denchic45/studiversity/ui/yourworks/YourWorksUI.kt
@@ -1,44 +1,21 @@
 package com.denchic45.studiversity.ui.yourworks
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Assignment
 import androidx.compose.material.icons.outlined.Task
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ScrollableTabRow
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Tab
+import androidx.compose.material3.*
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
-import androidx.compose.material3.Text
 import androidx.compose.material3.pullrefresh.PullRefreshIndicator
 import androidx.compose.material3.pullrefresh.PullRefreshState
 import androidx.compose.material3.pullrefresh.pullRefresh
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.snapshotFlow
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -55,7 +32,7 @@ import com.denchic45.studiversity.ui.uiTextOf
 import com.denchic45.stuiversity.api.course.work.model.CourseWorkResponse
 import com.denchic45.stuiversity.util.toString
 import kotlinx.coroutines.launch
-import java.util.UUID
+import java.util.*
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -69,7 +46,7 @@ fun YourWorksScreen(component: YourWorksComponent) {
     Surface {
         Column(Modifier.fillMaxSize()) {
             val selected by component.selectedTab.collectAsState()
-            val pagerState = rememberPagerState(selected)
+            val pagerState = rememberPagerState(selected, pageCount = { children.size })
             LaunchedEffect(pagerState) {
                 snapshotFlow { pagerState.currentPage }.collect { page ->
                     component.selectedTab.value = page
@@ -95,7 +72,7 @@ fun YourWorksScreen(component: YourWorksComponent) {
                 }
             }
 
-            HorizontalPager(state = pagerState, pageCount = children.size) {
+            HorizontalPager(state = pagerState) {
                 Box(modifier = Modifier.fillMaxSize()) {
                     when (val child = children[it]) {
                         is YourWorksComponent.TabChild.Upcoming -> YourUpcomingWorksScreen(child.component)
@@ -145,7 +122,8 @@ fun WorksListContent(
                 Box(
                     Modifier
                         .fillMaxSize()
-                        .verticalScroll(state))
+                        .verticalScroll(state)
+                )
             }
         }
         PullRefreshIndicator(refreshing, refreshState, Modifier.align(Alignment.TopCenter))
